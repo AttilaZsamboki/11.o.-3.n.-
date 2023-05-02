dac04177585faf25b844a2a6a8d179c364cdedb2
AttilaZsamboki <99214950+AttilaZsamboki@users.noreply.github.com>
2023-05-02 12:01:53 +0200

    3. feladat kész

---
+++ 3_feladat.py
@@ -0,0 +1,34 @@
+# Összeadásjel működése
+a = 5
+b = 3
+c = a + b
+print(c)    # eredmény: 8
+
+s1 = "hello"
+s2 = "world"
+s3 = s1 + s2
+print(s3)   # eredmény: "helloworld"
+
+# Szorzásjel működése
+x = 2
+y = 3
+z = x * y
+print(z)    # eredmény: 6
+
+s = "abc"
+s = s * 3
+print(s)    # eredmény: "abcabcabc"
+
+"""
+
+A fenti programban először az összeadásjel és a szorzásjel működését teszteljük különböző típusú adatokkal. 
+Az összeadásjel két számot ad össze, vagy két karakterláncot fűz össze, míg a szorzásjel két számot szoroz össze, vagy egy karakterláncot többször ismétel.
+
+A tanulságok:
+
+ - A műveleti jelek olyan jelek, amelyekkel műveleteket végezhetünk az adatokkal.
+ - Az összeadásjel két számot ad össze, vagy két karakterláncot fűz össze.
+ - A szorzásjel két számot szoroz össze, vagy egy karakterláncot többször ismétel.
+ - A műveleti jel pontos működése attól függ, hogy az adat típusa milyen. Ezért fontos pontosan megadni az adat típusát a műveletek végrehajtása során.
+
+"""
