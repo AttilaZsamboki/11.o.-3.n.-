6d48b324b8667dba26a328c85bd1c7e2e97376f0
AttilaZsamboki <99214950+AttilaZsamboki@users.noreply.github.com>
2023-05-02 11:45:35 +0200

    1 feladat kész

---
+++ 1_feladat.py
@@ -0,0 +1,19 @@
+állat = 'ló'
+ló = 'Ráró'
+állat = 'macska'
+print(állat)
+# --- out: macska --- #
+
+gyümölcs = 'alma'
+gyümölcs = 'körte'
+alma = 'dinnye'
+dinnye = 3
+gyümölcs = alma
+print(gyümölcs)
+# --- out: dinnye --- #
+
+autó = 'Trabant'
+autó = 'Renault'
+autó = 3 - 5
+print(autó)
+# --- out: -2 --- #
